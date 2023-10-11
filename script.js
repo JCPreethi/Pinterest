bc7cca9e06f47409ea2a43bd13d9cc5aca7e4d00
unknown <preethi.c@datasemantics.co>
2023-10-11 11:01:36 +0530

    third screen and script changes

---
+++ script.js
@@ -0,0 +1,35 @@
+const sections = document.querySelectorAll('.section');
+
+const handleScroll = () => {
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    console.log(sectionHeight)
+    if (window.scrollY >= sectionTop - sectionHeight / 2) {
+      section.classList.add('active');
+    } else {
+      section.classList.remove('active');
+    }
+  });
+};
+
+document.addEventListener('scroll', handleScroll);
+
+
+const scrollButton = document.getElementById('pageDown');
+const scrolltoTopButton = document.getElementById('pageUp');
+const secondContainer = document.getElementById('secondcontainer');
+const firstContainer = document.getElementById('firstcontainer');
+
+scrollButton.addEventListener('click', scrollToSecondContainer);
+scrolltoTopButton.addEventListener('click',scrollToTop);
+
+
+function scrollToSecondContainer() {
+  secondContainer.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToTop(){
+  firstContainer.scrollIntoView({ behavior: 'smooth'});
+}
+
